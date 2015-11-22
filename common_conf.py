03faa126f564180464567ecedfa6f7226e86f3f7
Jiří Janoušek <janousek.jiri@gmail.com>
2015-11-22 11:10:56 +0100

    Use https for tiliado.github.io
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- common_conf.py
+++ common_conf.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 SITENAME = 'Nuvola Player Development'
 TILIADOWEB = "https://tiliado.eu/"
-TILIADO_IO = "http://tiliado.github.io/"
-TILIADO_IO_NUVOLA = "http://tiliado.github.io/nuvolaplayer/"
+TILIADO_IO = "https://tiliado.github.io/"
+TILIADO_IO_NUVOLA = "https://tiliado.github.io/nuvolaplayer/"
 INTERLINKS = {
     "tiliado": TILIADOWEB,
     "gh": "https://github.com/",
