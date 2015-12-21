19ed4f81dd88a61d5a6906ef7b28c9e725d99566
Jiří Janoušek <janousek.jiri@gmail.com>
2015-12-21 21:32:16 +0100

    Add CONSTANTS to conf
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- common_conf.py
+++ common_conf.py
@@ -5,9 +5,18 @@ SITENAME = 'Nuvola Player Development'
 TILIADOWEB = "https://tiliado.eu/"
 TILIADO_IO = "https://tiliado.github.io/"
 TILIADO_IO_NUVOLA = "https://tiliado.github.io/nuvolaplayer/"
+
 INTERLINKS = {
     "tiliado": TILIADOWEB,
     "gh": "https://github.com/",
     "ioroot": TILIADO_IO,
     "ionuvola": TILIADO_IO_NUVOLA,
+}
+
+CONSTANTS = {
+    "SITENAME": SITENAME,
+    "TILIADOWEB": TILIADOWEB,
+    "TILIADO_IO": TILIADO_IO,
+    "TILIADO_IO_NUVOLA": TILIADO_IO_NUVOLA,
+    "DEFAULT_LANG": "en",
 }
